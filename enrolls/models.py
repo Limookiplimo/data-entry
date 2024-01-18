730d91971394de6f982c2b232fcda5f9fd28d3d5
limoo <limoodrice@gmail.com>
2024-01-18 21:01:39 +0300

    created employee model

--- enrolls/models.py
+++ enrolls/models.py
@@ -17,4 +17,4 @@ class Employee(models.Model):
     department = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.first_name + '' + self.last_name
+        return self.first_name + ' ' + self.last_name

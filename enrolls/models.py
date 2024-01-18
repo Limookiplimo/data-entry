6864c18cce7a72ac076009cde04ea5ae02e28563
limoo <limoodrice@gmail.com>
2024-01-18 18:55:52 +0300

    first commit

---
+++ enrolls/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    TITLE = (
+        ('Mr', 'Mr'),
+        ('Mrs', 'Mrs'),
+        ('Miss', 'Miss'),
+        ('Dr', 'Dr'),
+        ('Prof', 'Prof'),
+    )
+    title = models.CharField(max_length=50, null=True, choices=TITLE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    department = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.first_name + '' + self.last_name

28e51db20f2b440485073484a95b1fa010cc703a
limoo <limoodrice@gmail.com>
2024-01-21 15:56:08 +0300

    Updated the view to always return empty form after a user clicks submit button

--- enrolls/views.py
+++ enrolls/views.py
@@ -9,12 +9,7 @@ def enroll_employee(request):
         form = EmployeeForm(request.POST)
         if form.is_valid():
             form.save()
+            form = EmployeeForm()
 
     context = {"form":form}
     return render(request, "enrolls/enroll.html", context)
-
-def user_login():
-    pass
-
-def register_user():
-    pass

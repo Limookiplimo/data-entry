2baa2b2a65973796eb9e72e6fa4a1cfa04f4f832
limoo <limoodrice@gmail.com>
2024-01-22 19:51:08 +0300

    deployment configuration set-ups

--- data_entry/urls.py
+++ data_entry/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('enrolls/', include("enrolls.urls"))
+    path('', include("enrolls.urls"))
 ]
